179912a46e4f7a1cc9b15c7256748b384c2d4473
frlopez88 <frlopez88@icloud.com>
2026-02-20 20:13:41 -0600

    Commit

--- src/components/ChatBotForm.jsx
+++ src/components/ChatBotForm.jsx
@@ -2,6 +2,34 @@ import React, { useEffect, useState } from 'react'
 
 export const ChatBotForm = () => {
 
+    const  [products , setProducts] = useState("");
+    const getProductList = async () => {
+
+        const url = import.meta.env.VITE_SUPABASE_URL + 'products';
+        const token = import.meta.env.VITE_TOKEN;
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                'apikey': token
+            }
+        })
+
+        const data = await response.json();
+        let prods=""
+        for (let i =0; i< data.length; i++){
+            prods += `[${i+1}) ${data[i].name} - $ ${data[i].price}]`
+        }
+
+
+        setProducts(prods)
+        
+
+    }
+
+    useEffect(()=>{
+        getProductList()
+    }, [])
+
     const [messages, setMessages] = useState([])
     let question = "";
 
@@ -11,7 +39,31 @@ export const ChatBotForm = () => {
 
     }
 
-    const [loading , setLoading] = useState(false)
+
+
+    const [loading, setLoading] = useState(false);
+
+    const instructions = `
+    You are going be a helpfull assintant in my online store
+    
+    The product list is:
+
+    ${products}
+
+    You are going to take the order products and the request to our staff. 
+
+    Always confirm the order
+
+    When the order is confirmed say Good Bye, and give a summary of the total!
+
+    Format Rule: Only respond in plain text. Don't use any format like HTML or Mark Down. Don't use Bold or Italic fonts. 
+
+    You can use emojis.
+
+    
+    `
+
+    console.log(instructions)
 
     const onSubmitForm = async () => {
 
@@ -24,13 +76,22 @@ export const ChatBotForm = () => {
 
         setMessages(history)
 
-        const contents = {
-            parts: [{ text: question }]
-        }
+        //const contents = {
+        //    parts: [{ text: question }]
+        //}
 
         event.preventDefault();
 
         setLoading(true)
+
+        // History Context to the AI
+
+        const apiHistory = history.map(item => ({
+            role: item.role,
+            parts: [{ text: item.text }]
+        }))
+
+        console.log(apiHistory)
 
         const result = await fetch(url, {
             method: "POST",
@@ -38,11 +99,15 @@ export const ChatBotForm = () => {
                 'x-goog-api-key': token
             },
             body: JSON.stringify(
-                { contents: contents }
+                {
+                    system_instruction: { parts: [{ text: instructions }] },
+                    contents: apiHistory
+                }
             )
         })
 
         const data = await result.json();
+        console.log(data)
 
         const answer = data.candidates[0].content.parts[0].text;
         console.log(answer)
@@ -63,14 +128,14 @@ export const ChatBotForm = () => {
                 <div className='mt-3 mb-3 border rounded-3'>
                     {
                         messages.map((item, i) => (
-                            <p key={i} className={ item.role == "model" ? "text-success": "text-dark" }  > {item.text} </p>
+                            <p key={i} className={item.role == "model" ? "text-success" : "text-dark"}  > {item.text} </p>
 
                         ))
                     }
 
                 </div>
-                
-                { loading ? <p> Loading ... </p> : <></> }
+
+                {loading ? <p> Loading ... </p> : <></>}
 
                 <div className="mb-3">
                     <input type="text" className="form-control" onChange={onChangeQuestion}
